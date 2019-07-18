eb0e546534b4ef3b3978b9c2511a3a20edba7d09
GCLemon <green.c.lemon@gmail.com>
2019-07-18 23:09:36 +0900

    アセンブリ名を変更

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 cd library/rational
 sh build.sh
 cd ../..
-g++ -std=c++17 -O -o product/8bit \
+g++ -std=c++17 -O -o product/mmltofc \
     `find ./source -name "*.cpp"` \
     ./library/rational/product/rational.a
-cd product
-./8bit -o test.wav sample.mml
-cd ..
